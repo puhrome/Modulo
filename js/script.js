83ff4c90773e94ccb75abb4439d45a2cb22c7170
puhrome <phirom.yim@fullsail.edu>
2014-04-13 16:01:42 -0400

    Module
    Using the % to create remainders in WebStorm for HTML and JS

---
+++ js/script.js
@@ -0,0 +1,6 @@
+/**
+ * Modulo Created by Phirom Yim on 4/13/14.
+ */
+var remainder = 37 % 3; //% is used for remainders
+//good thing to use if you want to know if a number is odd or even
+console.log(remainder);
